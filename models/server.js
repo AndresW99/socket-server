15b7c5eda278447bfaa1331e54976e146ead5d5a
Andres Wellmann <wellmann39@gmail.com>
2021-01-04 20:17:20 -0600

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,53 @@
+//Servidor de Express
+const express  = require('express');
+const http     = require('http');
+const socketio = require('socket.io');
+const path     = require('path');
+
+const Sockets = require('./sockets');
+
+class Server {
+
+    constructor() {
+
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // Http server
+        this.server = http.createServer( this.app );
+
+        // Configuracion del socket server
+        this.io = socketio( this.server, { /* configuraciones */ } );
+    }
+
+    middlewares() {
+
+        // Desplegar el directorio publico
+        this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
+
+    }
+
+    configurarSockets() {
+
+        new Sockets( this.io );
+
+    }
+
+    execute() {
+
+        //Inicializar Middlwares
+        this.middlewares();
+
+        //Inicializar Sockets
+        this.configurarSockets();
+
+        // Inicializar server
+        this.server.listen(this.port, () => {
+            console.log('Server corriendo en el puerto: ', this.port );
+        });
+
+    }
+
+}
+
+module.exports = Server;
